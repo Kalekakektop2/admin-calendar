dbae27974e4c10205e10950d683d6d8bd05b6c50
Kalekakektop2 <segakiller0071@gmail.com>
2026-07-24 05:21:05 +0300

    Fix next.config.ts remove deprecated options

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        '@': './src',
-      },
-    },
-  },
 };
 
 export default nextConfig;
